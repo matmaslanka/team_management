ce48f0ebce84df40de5c4e0368d4a2c9da428ad0
matmaslanka <mates94@gmail.com>
2024-03-21 15:06:21 +0100

    Backend finished

--- team/urls.py
+++ team/urls.py
@@ -4,7 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.EmployeesView.as_view(), name='index'),
     path('tasks/add', views.AddTaskView.as_view(), name='add_task'),
+    path('employees/add_new_employee',
+         views.AddNewEmployeeView.as_view(), name='add_new_employee'),
+    path('employees/<int:pk>/add-task/',
+         views.AddTaskToEmployeeView.as_view(), name='add_task_to_employee'),
+    path('employees/<int:pk>/update/',
+         views.UpdateEmployeeView.as_view(), name='update_employee'),
     path('employees/<int:pk>', views.TasksView.as_view(), name='employee_id'),
-    path('tasks/<int:pk>', views.SingleTaskView.as_view(), name='task_id')
-
+    path('tasks/<int:pk>', views.SingleTaskView.as_view(), name='task_id'),
+    path('tasks/<int:pk>/update/', views.UpdateTaskView.as_view(), name='update_task')
 ]
--- team/views.py
+++ team/views.py
@@ -1,23 +1,80 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView
+from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 from .models import Employee, Task
 
 # Create your views here.
+
 
 class EmployeesView(ListView):
     template_name = 'team/employees_list.html'
     model = Employee
     context_object_name = 'employees'
 
+
 class AddTaskView(CreateView):
     model = Task
     fields = '__all__'
     template_name = 'team/add_task.html'
     success_url = '/'
+
+
+class AddNewEmployeeView(CreateView):
+    model = Employee
+    fields = '__all__'
+    template_name = 'team/add_new_employee.html'
+    success_url = '/'
+
+
+class UpdateEmployeeView(UpdateView):
+    model = Employee
+    fields = ['employee_task']
+    template_name = 'team/add_task_to_employee.html'
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        employee_id = self.kwargs.get('pk')
+        context['employee_id'] = employee_id
+        return context
+
+
+class AddTaskToEmployeeView(CreateView):
+    model = Task
+    fields = '__all__'
+    template_name = 'team/add_task_to_employee.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        employee_id = self.kwargs.get('id')
+        context['employee_id'] = employee_id
+        return context
+
+    def form_valid(self, form):
+        employee = Employee.objects.get(pk=self.kwargs['pk'])
+        task = form.save()
+        employee.employee_task.add(task)
+        return super().form_valid(form)
+
+    success_url = '/'
+
+
+class UpdateTaskView(UpdateView):
+    model = Task
+    fields = '__all__'
+    template_name = 'team/task_update.html'
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        task_id = self.kwargs.get('pk')
+        context['task_id'] = task_id
+        return context
 
 
 class TasksView(DetailView):
