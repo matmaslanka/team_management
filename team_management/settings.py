20b59b041c2f425afdd4ddb6e61e19ce855ab3d6
Mateusz Maślanka <106690131+matmaslanka@users.noreply.github.com>
2024-10-24 14:08:05 +0200

    Update settings.py
    Hiding the secret key

--- team_management/settings.py
+++ team_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6hd=-g6&dcx%@oea1$)s4r0xv+o9r6*-3$4mjc$1bd1yje&fv8'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
